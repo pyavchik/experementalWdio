0beeba2efe8a625725220e6a357da3eb48d6da82
Grigoriy Stetsenko <stetsenko@softesis.com>
2018-11-30 10:59:07 +0200

    fix adding package.json and wdio.conf.js

---
+++ wdio.conf.js
@@ -0,0 +1,19 @@
+exports.config = {
+    specs: [
+        './test/specs/**/*.js'
+    ],
+    maxInstances: 10,
+    capabilities: [{
+        maxInstances: 5,
+        browserName: 'chrome'
+    }],
+    sync: true,
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 90000,
+    connectionRetryCount: 3,
+    services: ['selenium-standalone'],
+    framework: 'mocha',
+    mochaOpts: {
+        ui: 'bdd'
+    }
+}
